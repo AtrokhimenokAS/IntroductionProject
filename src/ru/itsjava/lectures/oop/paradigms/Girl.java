804c673c9bb419247087e40bc4fe06c50264e0d0
AtrokhimenokAS <fizik86@mail.ru>
2020-01-30 02:35:54 +0300

    add girl

--- src/ru/itsjava/lectures/oop/paradigms/Girl.java
+++ src/ru/itsjava/lectures/oop/paradigms/Girl.java
@@ -1,13 +1,60 @@
 package ru.itsjava.lectures.oop.paradigms;
 
+import java.util.Objects;
+
 public class Girl {
     private String name;
     private double age;
+    private double height;
     private String telephone;
 
-    public Girl(String name, double age, String telephone) {
+    public Girl(String name, double age, double height,String telephone) {
         this.name = name;
         this.age = age;
+        this.height = height;
         this.telephone = telephone;
     }
+
+    @Override
+    public String toString() {
+        return "name= " + name  +
+                ", age=" + age +
+                ", height=" + height +
+                ", telephone=" + telephone + '\n';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Girl girl = (Girl) o;
+        if (name == null) {
+            if (girl.name != null)
+                return false;
+        } else {
+            if (!name.equals(girl.name))
+                return false;
+        }
+        if (age != girl.age)
+            return false;
+        if (height != girl.height)
+            return false;
+        if (telephone == null) {
+            return girl.telephone == null;
+        } else {
+            return telephone.equals(girl.telephone);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 96 * result + (int) (age * height);
+        result += ((name == null) ? 0 : name.hashCode());
+        result += ((telephone == null) ? 0 : telephone.hashCode());
+        return result;
+
+    }
+
 }
+
