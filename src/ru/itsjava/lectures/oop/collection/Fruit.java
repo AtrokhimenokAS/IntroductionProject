85309883aeef3385f49c44d5db1476e643413c14
AtrokhimenokAS <fizik86@mail.ru>
2020-03-03 19:33:39 +0300

    mapa practice

---
+++ src/ru/itsjava/lectures/oop/collection/Fruit.java
@@ -0,0 +1,51 @@
+package ru.itsjava.lectures.oop.collection;
+
+import java.util.Objects;
+
+public class Fruit {
+    private String name;
+    private double weight;
+
+    public Fruit(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fruit fruit = (Fruit) o;
+        return Double.compare(fruit.weight, weight) == 0 &&
+                Objects.equals(name, fruit.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, weight);
+    }
+}
